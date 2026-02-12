dd6ab137c568e13a31ce955773834f921ca8fa28
William Li <willli@MacBook-Pro-4.local>
2026-02-12 10:32:55 -0500

    Retinal genes RO

--- R scripts/Heatmaps_gene_sets.R
+++ R scripts/Heatmaps_gene_sets.R
@@ -1,46 +1,47 @@
 library(ComplexHeatmap)
-library(countToFPKM)
 library(DESeq2)
+library(circlize)
 library(tidyverse)
-library(grid)
-library(biomaRt)
+library(org.Hs.eg.db)
+library(AnnotationDbi)
 
-# RO_full <- read.table("C:/Users/willllllli/Documents/Dr. Z lab/RNA seq/RO/Counts/count.out",
-#                      header = T,
-#                      sep = "\t",
-#                      comment.char = "#",
-#                      stringsAsFactors = F)
-# 
-# RO_counts <- RO_full[, 7:ncol(RO_full)]
-# rownames(RO_counts) <- RO_full$Geneid
-# colnames(RO_counts) = c("H9_E8_r1", "H9_E8_r2",  "H9_L3i_r1", "H9_L3i_r2", 
-#                             "RUES02_E8", "RUES02_L3i")
-# RO_counts <- RO_counts[, c("H9_E8_r1", "H9_E8_r2", "RUES02_E8", "H9_L3i_r1", "H9_L3i_r2", "RUES02_L3i")]
-# featureLength <- RO_full$Length
-# 
-# ff <- fpkm(RO_counts, featureLength, meanFragmentLength = 250)
+# RO_full <- read.table("C:/Users/willllllli/Documents/Dr. Z lab/RNA seq/RO/Counts/count.out", # Windows
+RO_full <- read.table("~/Documents/Zambidis lab/RNAseq/RO/Counts/count.out",
+                     header = T,
+                     sep = "\t",
+                     comment.char = "#",
+                     stringsAsFactors = F)
 
-
-RO_counts <- read.csv("C:/Users/willllllli/Documents/Dr. Z lab/RNA seq/RO/Counts/RO_counts_gene_symbol.csv")
-rownames(RO_counts) <- RO_counts$X
-RO_counts$X <- NULL
+RO_counts <- RO_full[, 7:ncol(RO_full)]
+rownames(RO_counts) <- RO_full$Geneid
+colnames(RO_counts) = c("H9_E8_r1", "H9_E8_r2",  "H9_L3i_r1", "H9_L3i_r2",
+                            "RUES02_E8", "RUES02_L3i")
 RO_counts <- RO_counts[, c("H9_E8_r1", "H9_E8_r2", "RUES02_E8", "H9_L3i_r1", "H9_L3i_r2", "RUES02_L3i")]
+featureLength <- RO_full$Length
 
-genes <- read.csv("C:/Users/willllllli/Documents/Dr. Z lab/RNA seq/RO/Retinal_genes.csv")
+ens <- rownames(RO_counts)
+symbols <- mapIds(org.Hs.eg.db,
+                  keys = ens,
+                  keytype = "ENSEMBL",
+                  column = "SYMBOL",
+                  multiVals = "first")
+RO_counts$gene_symbol <- symbols
+rownames(RO_counts) <- make.unique(ifelse(is.na(symbols), ens, symbols))
+RO_counts$gene_symbol <- NULL
+
+genes <- read.csv("~/Documents/Zambidis lab/RNAseq/RO/Retinal_genes.csv") # MAC
+genes <- genes[, c("Retinal.Stem.Cell", "Ganglion", "HA.cell", "Photoreceptor", "Bipolar","Muller") ]
 
 # Differential expression ----------------------------------
 samples <- colnames(RO_counts)
 cellline <- sub("_(E8|L3i)?", "", samples)
 condition <- sub(".*_(E8|L3i).*", "\\1", samples)
-
 coldata <- data.frame(
   cellline = factor(cellline),
   condition = factor(condition, levels = c("E8", "L3i")),
   row.names = samples
 )
-
 all(colnames(RO_counts) == rownames(coldata))
-
 dds <-DESeqDataSetFromMatrix(
   countData = RO_counts,
   colData = coldata,
@@ -54,6 +55,16 @@ mat <- counts(dds, normalized = TRUE)
 mat_scaled <- t(scale(t(mat)))
 mat_scaled <- mat_scaled[complete.cases(mat_scaled), , drop = FALSE]
 
+# TPM calculation -------------------
+
+rpk <- RO_counts / (featureLength / 1000)  # Reads per kilobase
+tpm <- t(t(rpk) / colSums(rpk) * 1e6)  # TPM normalization
+
+# RO_norm <- RO_counts/colSums(RO_counts) * 1e6
+# rlog_counts <- log2(RO_norm/(rowMeans(RO_norm) + 1 ))
+
+rld <- rlog(dds, blind = FALSE)
+rlog_counts <- assay(rld)
 
 
 # Heatmap plotting --------------------------------------
@@ -66,6 +77,7 @@ for (i in seq_along(celltypes)) {
   celltype <- celltypes[i]
   gene_set <- genes[[i]]
   sub <- mat_scaled[rownames(mat_scaled) %in% gene_set, ]
+  # sub <- tpm[rownames(tpm) %in% gene_set, ]
 
   E8_cols  <- grepl("_E8", colnames(sub))
   L3i_cols <- grepl("_L3i", colnames(sub))
@@ -76,6 +88,8 @@ for (i in seq_along(celltypes)) {
     E8_mean  = rowMeans(sub[, E8_cols]),
     L3i_mean = rowMeans(sub[, L3i_cols])
   )
+  gene_ord <- order(heatmap_mat[, 'L3i_mean'], decreasing = TRUE)
+  heatmap_mat <- heatmap_mat[gene_ord, ]
   all_gene_sets[[i]] <- heatmap_mat
   slice_labels <- c(slice_labels, rep(celltype, nrow(heatmap_mat)))
 }
@@ -84,16 +98,16 @@ combined_mat <- do.call(rbind, all_gene_sets)
 
 ht_combined <- Heatmap(
   combined_mat,
-  name = "z-score",
+  name = "r-log",
   show_row_names = TRUE, 
   show_column_names = TRUE, 
   cluster_columns = FALSE, 
-  cluster_rows = TRUE,
+  cluster_rows = FALSE,
   cluster_row_slices = FALSE,
-  row_split = factor(slice_labels, levels = celltypes),  # This creates the slices
+  row_split = factor(slice_labels, levels = celltypes),
   row_title_rot = 0,
   row_gap = unit(2, "mm"),
-  row_names_gp = gpar(fontsize = 9),
+  row_names_gp = gpar(fontsize = 8),
   column_names_gp = gpar(fontsize = 12),
   row_title_gp = gpar(fontsize = 14, fontface = "bold")
 )
