2876ca787c86e0bb9041e38ebcdc1fbbea4c6d11
William Li <willli@MacBook-Pro-4.local>
2026-02-17 16:48:33 -0500

    create figure diff/undiff heatmaps

--- R scripts/Heatmaps_gene_sets.R
+++ R scripts/Heatmaps_gene_sets.R
@@ -4,6 +4,7 @@ library(circlize)
 library(tidyverse)
 library(org.Hs.eg.db)
 library(AnnotationDbi)
+library(patchwork)
 
 # RO_full <- read.table("C:/Users/willllllli/Documents/Dr. Z lab/RNA seq/RO/Counts/count.out", # Windows
 RO_full <- read.table("~/Documents/Zambidis lab/RNAseq/RO/Counts/count.out",
@@ -14,9 +15,9 @@ RO_full <- read.table("~/Documents/Zambidis lab/RNAseq/RO/Counts/count.out",
 
 RO_counts <- RO_full[, 7:ncol(RO_full)]
 rownames(RO_counts) <- RO_full$Geneid
-colnames(RO_counts) = c("H9_E8_r1", "H9_E8_r2",  "H9_L3i_r1", "H9_L3i_r2",
+colnames(RO_counts) = c("H9_r1_E8", "H9_r2_E8",  "H9_r1_L3i", "H9_r2_L3i",
                             "RUES02_E8", "RUES02_L3i")
-RO_counts <- RO_counts[, c("H9_E8_r1", "H9_E8_r2", "RUES02_E8", "H9_L3i_r1", "H9_L3i_r2", "RUES02_L3i")]
+RO_counts <- RO_counts[, c("H9_r1_E8", "H9_r2_E8", "RUES02_E8", "H9_r1_L3i", "H9_r2_L3i", "RUES02_L3i")]
 featureLength <- RO_full$Length
 
 ens <- rownames(RO_counts)
@@ -29,12 +30,29 @@ RO_counts$gene_symbol <- symbols
 rownames(RO_counts) <- make.unique(ifelse(is.na(symbols), ens, symbols))
 RO_counts$gene_symbol <- NULL
 
-genes <- read.csv("~/Documents/Zambidis lab/RNAseq/RO/Retinal_genes.csv") # MAC
-genes <- genes[, c("Retinal.Stem.Cell", "Ganglion", "HA.cell", "Photoreceptor", "Bipolar","Muller") ]
+undiff_genes <- read.csv("~/Documents/Zambidis lab/RNAseq/RO/Retinal_progenitor_genes.csv") # MAC
+diff_genes <- read.csv("~/Documents/Zambidis lab/RNAseq/RO/Retinal_differentiated_genes.csv") # MAC
+
+undiff_cell_types <- unique(gsub("\\.(Liu|Dorgau)$", "", names(diff_genes)))
+
+diff_genes_list <- lapply(undiff_cell_types, function(ct) {
+  matching_cols <- grep(paste0("^", ct, "\\.(Liu|Dorgau)$"), names(diff_genes), value = TRUE)
+  unique(na.omit(unlist(diff_genes[matching_cols])))
+})
+
+names(diff_genes_list) <- undiff_cell_types
+max_len <- max(sapply(diff_genes_list, length))
+
+diff_genes_df <- as.data.frame(
+  lapply(diff_genes_list, function(x) {
+    length(x) <- max_len  # pad with NA
+    x
+  })
+)
 
 # Differential expression ----------------------------------
 samples <- colnames(RO_counts)
-cellline <- sub("_(E8|L3i)?", "", samples)
+cellline <- sub("_(E8|L3i)", "", samples)
 condition <- sub(".*_(E8|L3i).*", "\\1", samples)
 coldata <- data.frame(
   cellline = factor(cellline),
@@ -55,62 +73,60 @@ mat <- counts(dds, normalized = TRUE)
 mat_scaled <- t(scale(t(mat)))
 mat_scaled <- mat_scaled[complete.cases(mat_scaled), , drop = FALSE]
 
-# TPM calculation -------------------
-
-rpk <- RO_counts / (featureLength / 1000)  # Reads per kilobase
-tpm <- t(t(rpk) / colSums(rpk) * 1e6)  # TPM normalization
-
-# RO_norm <- RO_counts/colSums(RO_counts) * 1e6
-# rlog_counts <- log2(RO_norm/(rowMeans(RO_norm) + 1 ))
-
-rld <- rlog(dds, blind = FALSE)
-rlog_counts <- assay(rld)
-
-
 # Heatmap plotting --------------------------------------
-celltypes <- names(genes)
-
-all_gene_sets <- list()
-slice_labels <- c()
-
-for (i in seq_along(celltypes)) {
-  celltype <- celltypes[i]
-  gene_set <- genes[[i]]
-  sub <- mat_scaled[rownames(mat_scaled) %in% gene_set, ]
-  # sub <- tpm[rownames(tpm) %in% gene_set, ]
-
-  E8_cols  <- grepl("_E8", colnames(sub))
-  L3i_cols <- grepl("_L3i", colnames(sub))
+make_heatmap <- function(mat_scaled, genes, title){
+  all_gene_sets <- list()
+  slice_labels <- c()
+  celltypes <- names(genes)
   
-  heatmap_mat <- cbind(
-    sub[, E8_cols],
-    sub[, L3i_cols],
-    E8_mean  = rowMeans(sub[, E8_cols]),
-    L3i_mean = rowMeans(sub[, L3i_cols])
+  for (i in seq_along(celltypes)) {
+    celltype <- celltypes[i]
+    gene_set <- genes[[i]]
+    sub <- mat_scaled[rownames(mat_scaled) %in% gene_set, ]
+    for (g in gene_set){
+      if (!g %in% gene_set) {print(g)}
+    }
+    
+    # sub <- tpm[rownames(tpm) %in% gene_set, ]
+    
+    E8_cols  <- grepl("_E8", colnames(sub))
+    L3i_cols <- grepl("_L3i", colnames(sub))
+    
+    heatmap_mat <- cbind(
+      # sub[, E8_cols],
+      # sub[, L3i_cols],
+      E8_mean  = rowMeans(sub[, E8_cols]),
+      L3i_mean = rowMeans(sub[, L3i_cols])
+    )
+    gene_ord <- order(heatmap_mat[, 'L3i_mean'], decreasing = TRUE)
+    heatmap_mat <- heatmap_mat[gene_ord, ]
+    all_gene_sets[[i]] <- heatmap_mat
+    slice_labels <- c(slice_labels, rep(celltype, nrow(heatmap_mat)))
+  }
+  
+  combined_mat <- do.call(rbind, all_gene_sets)
+  
+  ht_combined <- Heatmap(
+    combined_mat,
+    name = 'z-score',
+    show_row_names = TRUE, 
+    show_column_names = TRUE, 
+    cluster_columns = FALSE, 
+    cluster_rows = FALSE,
+    cluster_row_slices = FALSE,
+    row_split = factor(slice_labels, levels = celltypes),
+    row_title_rot = 0,
+    row_gap = unit(2, "mm"),
+    row_names_gp = gpar(fontsize = 8),
+    column_names_gp = gpar(fontsize = 12),
+    row_title_gp = gpar(fontsize = 14, fontface = "bold")
   )
-  gene_ord <- order(heatmap_mat[, 'L3i_mean'], decreasing = TRUE)
-  heatmap_mat <- heatmap_mat[gene_ord, ]
-  all_gene_sets[[i]] <- heatmap_mat
-  slice_labels <- c(slice_labels, rep(celltype, nrow(heatmap_mat)))
+  
+  draw(ht_combined,
+       show_heatmap_legend = T)
+  return(ht_combined)
 }
 
-combined_mat <- do.call(rbind, all_gene_sets)
+undiff_ht <- make_heatmap(mat_scaled, undiff_genes)
+diff_ht <- make_heatmap(mat_scaled, diff_genes_df)
 
-ht_combined <- Heatmap(
-  combined_mat,
-  name = "z-score",
-  show_row_names = TRUE, 
-  show_column_names = TRUE, 
-  cluster_columns = FALSE, 
-  cluster_rows = FALSE,
-  cluster_row_slices = FALSE,
-  row_split = factor(slice_labels, levels = celltypes),
-  row_title_rot = 0,
-  row_gap = unit(2, "mm"),
-  row_names_gp = gpar(fontsize = 8),
-  column_names_gp = gpar(fontsize = 12),
-  row_title_gp = gpar(fontsize = 14, fontface = "bold")
-)
-
-draw(ht_combined,
-     show_heatmap_legend = T)
